907dc5aa9e3a5d64f0bb97d8aaed5bfc08ddc13c
miyaviee <merveilles1998318@yahoo.co.jp>
2018-06-02 10:19:55 +0900

    add django celery setting

--- app/settings.py
+++
@@ -1,5 +0,0 @@
-CELERY_REDIS_HOST = 'localhost'
-CELERY_ACCEPT_CONTENT = ['pickle']
-CELERY_TASK_SERIALIZER = 'pickle'
-BROKER_URL = '{}://{}/{}'.format('redis', CELERY_REDIS_HOST, 1)
-INSTALLED_APPS = []
--- fuga/settings.py
+++ fuga/settings.py
@@ -162,3 +162,8 @@ CACHES = {
         }
     }
 }
+
+CELERY_REDIS_HOST = 'localhost'
+CELERY_ACCEPT_CONTENT = ['pickle']
+CELERY_TASK_SERIALIZER = 'pickle'
+BROKER_URL = '{}://{}/{}'.format('redis', CELERY_REDIS_HOST, 1)
