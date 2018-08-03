ff11c9fe7dc690f02cec071de6ec401dc000ed49
miyaviee <merveilles1998318@yahoo.co.jp>
2018-08-04 08:21:54 +0900

    add bjoern

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import bjoern
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fuga.settings")
+
+application = get_wsgi_application()
+
+bjoern.run(application, host='127.0.0.1', port=8000)
